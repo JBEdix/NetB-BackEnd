5d303b607275a3f2aae16ebd9a94b300869e4697
JB_Royale <jheral.blanco@gmail.com>
2020-12-10 22:12:04 -0600

    FIX: arreglo api User
    Se agregan campos al modelo y para la base de datos.

--- models/users.model.js
+++ models/users.model.js
@@ -8,6 +8,15 @@ const userSchema = mongoose.Schema({
     cart: {type:Array, require:true},
     pricingSelected: {type:Array, require:true},
     userStatus: {type:String, require:true},
-    billing: {type:Array, require:true}
+    billing: {type:Array, require:true},
+    address: {type:String, require:true},
+    address2: {type:String},
+    city: {type:String, require:true},
+    country: {type:String, require:true},
+    phone: {type:String, require:true},
+    webPage: {type:String},
+    zipCode: {type:String, require:true},
+    terms:{type: Boolean, require:true},
+    date:{type: Date, require: true}
 });
 module.exports = mongoose.model('users', userSchema);
